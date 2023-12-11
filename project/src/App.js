00d8ef2dfcf96a2a18434dc51be04b7ccc612ef7
Rutvikbhambhi <bhambhirutvik2004@gmail.com>
2023-12-11 15:50:41 +0530

    data

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,15 @@
 // import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import Slider from "./Slider"
+import Header from "./../src/Component/Header";
+import Slider from "./Component/Slider.jsx";
+
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 function App() {
   return (
    <>
+   <Header />
    <h1>hello</h1>
    <Slider/>
    </>
---
+++ project/src/Router/MainRouter.jsx
@@ -0,0 +1,7 @@
+import { createBrowserRouter } from 'react-router-dom';
+import HeaderCompo from '../Component/Header';
+const router=createBrowserRouter([
+    {
+        
+    }
+])
