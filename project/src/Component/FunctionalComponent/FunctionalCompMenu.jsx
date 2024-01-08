bdfe4d15ef405c4bd3ab21b88668fc1980299e49
rutvikbhambhi <bhambhirutvik2004@gmail.com>
2024-01-08 11:26:47 +0530

    data

--- project/src/Component/FunctionalComponent/FunctionalCompMenu.jsx
+++ project/src/Component/FunctionalComponent/FunctionalCompMenu.jsx
@@ -9,6 +9,16 @@ const FunctionalCompMenu=()=>{
                 <li><Link to="whatisfunctionalcomponent"> What is Functional Component </Link></li>
                 <li><Link to="stateinfunccomp"> State </Link></li>
                 <li><Link to="propsinfunccomp"> Props </Link></li>
+                <li><Link to="jsxinfunccomp"> JSX </Link></li>
+                <li><Link to="controlledcomponent"> Controlled Component </Link></li>
+                <li><Link to="controlledcomponentmultipledata"> Multiple Input in single State Component </Link></li>
+                <li><Link to="restandspread"> Rest Vs Spread </Link></li>
+                <li><Link to="hooksinfunctionalcompo"> Hooks </Link></li>
+                <li><Link to="useeffectinfunctionalcompo"> useEffect </Link></li>
+                <li><Link to="uselayouteffectinfunctionalcompo"> useLayoutEffect </Link></li>
+                <li><Link to="usereffectinfunctionalcompo"> useRef </Link></li>
+                <li><Link to="usecallbackinfunctionalcompo"> useCallback </Link></li>
+                <li><Link to="usememoinfunctionalcompo"> useMemo </Link></li>
             </ol>
             </div>
         </div>
--- project/src/Component/FunctionalComponent/FunctionalCompRoute.jsx
+++ project/src/Component/FunctionalComponent/FunctionalCompRoute.jsx
@@ -3,7 +3,17 @@ import { useRoutes } from "react-router-dom";
 import FunctionalCompMenu from "./FunctionalCompMenu";
 import FunctionalcompinReact from "./01FunctionalCompInReact";
 import StateFunctionalCompInReact from "./02StateFunctionalCompInReact";
-import PropsFunctionalComInReact from "./03PropsFunctionalComInReact"
+import PropsFunctionalComInReact from "./03PropsFunctionalComInReact";
+import JSXFunctionalComponentInReact from './05JSXFunctionalComponentInReact.jsx';
+import ControlledComponentInReact from './06ControlledComponentInReact.jsx';
+import MultipleDataComponentInReact from './07MultipleDataComponentInReact.jsx';
+import RestVsSpreadComponentInReact from './08RestVsSpreadComponentInReact.jsx';
+import HooksInFunctionalCompo from './09HooksInFunctionalCompo.jsx';
+import UseEffectHooksInFunctionalCompo from './10UseEffectHooksInFunctionalCompo.jsx';
+import UseLayoutEffectHooksInFunctionalCompo from './11UseLayoutEffectHooksInFunctionalCompo.jsx';
+import UseRefEffectHooksInFunctionalCompo from './12UseRefEffectHooksInFunctionalCompo.jsx';
+import UseCallbackHooksInFunctionalCompo from './13UseCallbackHooksInFunctionalCompo.jsx';
+import UseMemoHooksInFunctionalCompo from './14UseMemoHooksInFunctionalCompo.jsx';
 
 
 const FunctionalCompRoute = () => {
@@ -24,6 +34,36 @@ const FunctionalCompRoute = () => {
                 {
                     path:"propsinfunccomp",
                     element:< PropsFunctionalComInReact/>,
+                }, {
+                    path: "jsxinfunccomp",
+                    element: <JSXFunctionalComponentInReact />,
+                }, {
+                    path: "controlledcomponent",
+                    element: <ControlledComponentInReact />,
+                }, {
+                    path: "controlledcomponentmultipledata",
+                    element: <MultipleDataComponentInReact />,
+                }, {
+                    path: "restandspread",
+                    element: <RestVsSpreadComponentInReact />,
+                }, {
+                    path: "hooksinfunctionalcompo",
+                    element: <HooksInFunctionalCompo />,
+                }, {
+                    path: "useeffectinfunctionalcompo",
+                    element: <UseEffectHooksInFunctionalCompo />,
+                }, {
+                    path: "uselayouteffectinfunctionalcompo",
+                    element: <UseLayoutEffectHooksInFunctionalCompo />,
+                }, {
+                    path: "usereffectinfunctionalcompo",
+                    element: <UseRefEffectHooksInFunctionalCompo />,
+                }, {
+                    path: "usecallbackinfunctionalcompo",
+                    element: <UseCallbackHooksInFunctionalCompo />,
+                }, {
+                    path: "usememoinfunctionalcompo",
+                    element: <UseMemoHooksInFunctionalCompo />,
                 },
             ]
         },
