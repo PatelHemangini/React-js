cc76b96a573037a75b4215dfd52d3539bf1d3d43
PatelHemangini <hemanginipatel150120@yahoo.com>
2024-01-11 15:55:34 +0530

    done

--- Tutorial/script.js
+++ Tutorial/script.js
@@ -1,7 +1,41 @@
-function loadTutorial(tutorialId) {
-    var tutorialContent = document.getElementById('tutorial-content');
-    var tutorial = document.getElementById(tutorialId);
-  
-    tutorialContent.innerHTML = tutorial.innerHTML;
-  }
-  
+
+        // Check active classes
+        var checkClass = function () {
+            if ($('.item').hasClass('hide')) {
+                $('.item').removeClass('hide');
+            }
+        };
+        $(".html").addClass("active");
+        $(".item.html").removeClass("hide")
+        // Category filters
+        $('.item').click(function () {
+            checkClass();
+        });
+        $('.html').click(function () {
+            checkClass();
+            $('.item:not(.html)').toggleClass('hide');
+        });
+        $('.css').click(function () {
+            checkClass();
+            $('.item:not(.css)').toggleClass('hide');
+        });
+        $('.js').click(function () {
+            checkClass();
+            $('.item:not(.js)').toggleClass('hide');
+        });
+        $('.bootstrap').click(function () {
+            checkClass();
+            $('.item:not(.bootstrap)').toggleClass('hide');
+        });
+        $('.react').click(function () {
+            checkClass();
+            $('.item:not(.react)').toggleClass('hide');
+        });
+
+
+        // Active tag
+        $('.filter-btn').click(function () {
+            $('.filter-btn').removeClass('active');
+            $(this).addClass('active');
+        })
+    
